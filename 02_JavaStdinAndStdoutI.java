6c3d4900acd278a2022357721eec2f577b92f701
Rubesh Kanna <rubeshkanna1310@gmail.com>
2026-07-10 20:17:03 +0530

    Add HackerRank Java Introduction solutions

---
+++ 02_JavaStdinAndStdoutI.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Solution{
+ public static void main(String[] args){
+  Scanner scan=new Scanner(System.in);
+  int a=scan.nextInt(),b=scan.nextInt(),c=scan.nextInt();
+  System.out.println(a);
+  System.out.println(b);
+  System.out.println(c);
+  scan.close();
+ }
+}
