488540b18e532cbecb390740d159f8a4401c2560
Rubesh Kanna <rubeshkanna1310@gmail.com>
2026-08-26 18:46:23 +0530

    Time: 14 ms (28.47%), Space: 47 MB (14.78%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+
+        String rev = new StringBuilder(s).reverse().toString();
+
+        return s.equals(rev);
+    }
+}
